d002f6f501e544a0d1d8cb2ab14f08e222702b49
Raghu Betina <raghu@firstdraft.com>
2019-04-09 15:08:28 +0000

    Direct association between venues and top_picks

--- app/models/top_pick.rb
+++ app/models/top_pick.rb
@@ -1,6 +1,8 @@
 class TopPick < ApplicationRecord
   # Direct associations
 
+  belongs_to :venue
+
   belongs_to :user
 
   belongs_to :dish
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,6 +1,9 @@
 class Venue < ApplicationRecord
   # Direct associations
 
+  has_many   :top_picks,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
